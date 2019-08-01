cf3271796c5ab54f13f2443e7b56c6d73d80f43a
Saurabh Singh <saurabhsinghmay99@gmail.com>
2019-08-01 10:33:22 +0530

    refac

--- zombie.c
+++ zombie.c
@@ -1,15 +1,19 @@
 #include <stdlib.h> 
 #include <sys/types.h> 
 #include <unistd.h> 
+#include <stdio.h>
 int main() 
 { 
     pid_t child_pid = fork(); 
+
+    if (child_pid > 0) {
+        sleep(1);
+        printf("In parent process\n"); 
+    }
   
-    if (child_pid > 0) 
-        sleep(50); 
-  
-    else        
-        exit(0); 
-  
+    else{
+        printf("In child process\n");
+        exit(0);
+    }          
     return 0; 
 }
